5a66a23fa08c8fc6035fb5312c8c6559a87c6c85
Iván Gallego Torres <id.gallego16@iesdoctorbalmis.com>
2019-09-27 14:08:45 +0200

    Agregar archivos de proyecto.

---
+++ RatonTeclado/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text;
+
+namespace RatonTeclado
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            BotonDerechoRectangle.Fill = Brushes.White;
+            BotonIzquierdoRectangle.Fill = Brushes.White;
+        }
+
+        private void Ayuda(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+            {
+                ((TextBox)sender).Text = "AYUDA";
+            }
+        }
+
+        private void CambiarColor1(object sender, MouseButtonEventArgs e)
+        {
+            BotonIzquierdoRectangle.Fill = Brushes.Aqua;
+        }
+
+        private void RestaurarColor1(object sender, MouseButtonEventArgs e)
+        {
+            BotonIzquierdoRectangle.Fill = Brushes.White;
+        }
+
+        private void CambiarColor2(object sender, MouseButtonEventArgs e)
+        {
+            BotonDerechoRectangle.Fill = Brushes.Aqua;
+        }
+
+        private void RestaurarColor2(object sender, MouseButtonEventArgs e)
+        {
+            BotonDerechoRectangle.Fill = Brushes.White;
+        }
+
+        private void RatonSobre(object sender, MouseEventArgs e)
+        {
+            RatonSobreTextBox.Text = ((TextBox)sender).Name;
+        }
+
+        private void RatonSale(object sender, MouseEventArgs e)
+        {
+            RatonSobreTextBox.Text = "";
+        }
+
+        private void FocusSobreTextBox(object sender, RoutedEventArgs e)
+        {
+            FocoTextoBox.Text = ((TextBox)sender).Name;
+        }
+
+        private void LostFocusTextBox(object sender, RoutedEventArgs e)
+        {
+            FocoTextoBox.Text = "";
+        }
+
+        private void EliminarVocales(object sender, KeyEventArgs e)
+        {
+            
+        }
+    }
+}
